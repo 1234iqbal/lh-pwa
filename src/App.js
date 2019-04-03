c4c696573b5956596aae7cf8cccc692d45388945
user <mifjak1@gmail.com>
2019-04-03 10:12:23 +0700

    sama

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Landing_page from "./pages/Landing_page";
 import "./asset/css/master.css";
 class App extends Component {
   render() {
     return (
-      <div>
-        <h2>hello</h2>
-      </div>
+      <Router>
+        <div>
+          <Route path="/" exact component={Landing_page} />
+          <Route path="/Kampanye" exact component={Landing_page} />
+          <Route path="/Donasi" exact component={Landing_page} />
+          <Route path="/dll" exact component={Landing_page} />
+        </div>
+      </Router>
     );
   }
 }

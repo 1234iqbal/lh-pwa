58dbd633f539544bd02e8c801f713a90c060c50b
user <mifjak1@gmail.com>
2019-04-12 05:28:00 +0700

    update page login register

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Landing_page from "./pages/Landing_page";
 import Campaign from "./pages/Campaign";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import "./asset/css/master.css";
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route path="/" exact component={Landing_page} />
+          <Route path="/" exact component={Login} />
+          <Route path="/register" exact component={Register} />
           <Route path="/Kampanye" component={Campaign} />
           <Route path="/Donasi" component={Landing_page} />
           <Route path="/dll" component={Landing_page} />
---
+++ src/pages/Login/index.js
@@ -0,0 +1,71 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Typography from "@material-ui/core/Typography";
+
+const styles = {
+  root: {
+    paddingTop: "150px",
+    textAlign: "center",
+    backgroundColor: "none"
+  },
+  button: {
+    margin: 10,
+    width: 150,
+    backgroundColor: "#f50057",
+    color: "#fafafa",
+    fontFamily: "Kavoon"
+  },
+  text: {
+    color: "#045f50",
+    fontFamily: "Bevan"
+  },
+  textField: {}
+};
+
+function Index(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Typography variant="h4" className={classes.text} gutterBottom>
+        Lindungi Hutan
+      </Typography>
+
+      <TextField
+        id="outlined-email-input"
+        label="Email"
+        className={classes.textField}
+        type="email"
+        name="email"
+        autoComplete="email"
+        margin="normal"
+        variant="outlined"
+      />
+      <br />
+
+      <TextField
+        id="outlined-password-input"
+        label="Password"
+        className={classes.textField}
+        type="password"
+        autoComplete="current-password"
+        margin="normal"
+        variant="outlined"
+      />
+      <br />
+
+      <Button size="large" variant="contained" className={classes.button}>
+        Masuk
+      </Button>
+    </div>
+  );
+}
+
+Index.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Index);
